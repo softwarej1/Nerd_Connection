56d1056459989d142258c36c9ccd68ead30398d2
Obebe <shkim000905@naver.com>
2023-11-05 23:00:23 +0900

    Feat(modify_pfp):

--- lib/src/view/profile_modify.dart
+++ lib/src/view/profile_modify.dart
@@ -1,7 +1,6 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_getx_palette_diary/src/controller/profile_controller.dart';
+import 'package:flutter_getx_palette_diary/src/widget/modify_pfp.dart';
 import 'package:get/get.dart';
 
 class ProfileModify extends GetView<ProfileController> {
@@ -25,72 +24,30 @@ class ProfileModify extends GetView<ProfileController> {
   }
 
   Widget _body() {
-    return Column(
-      children: [
-        Center(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: GestureDetector(
-              onTap: () async {
-                await controller.pickImage();
-              },
-              child: Obx(
-                () {
-                  return Column(
-                    children: [
-                      ClipOval(
-                        child: controller.profileImagePath.value.isNotEmpty
-                            ? Image.file(
-                                File(controller.profileImagePath.value),
-                                width: 120.0,
-                                height: 120.0,
-                                fit: BoxFit.cover,
-                              )
-                            : Container(
-                                color: Colors.blue,
-                                width: 120.0,
-                                height: 120,
-                                child: const Center(
-                                  child: Icon(
-                                    Icons.camera_alt,
-                                    color: Colors.white,
-                                  ),
-                                ),
-                              ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 10.0),
-                        child: SizedBox(
-                          width: 150.0,
-                          child: TextFormField(
-                            textAlign: TextAlign.center,
-                            initialValue: '기존 이름',
-                          ),
-                        ),
-                      ),
-                    ],
-                  );
-                },
-              ),
-            ),
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Center(
+            child: PFP(controller: controller),
           ),
-        ),
-        ModifyBody(),
-        Column(
-          children: [
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  '내 소개',
-                  style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+          ModifyBody(),
+          Column(
+            children: [
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    '내 소개',
+                    style:
+                        TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+                  ),
                 ),
               ),
-            ),
-          ],
-        ),
-      ],
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
