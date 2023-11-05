13a21dcb0c321eb833869919f7be8529a4e9a990
Obebe <shkim000905@naver.com>
2023-11-05 21:15:26 +0900

    Refactor(profile):

--- lib/src/view/profile.dart
+++ lib/src/view/profile.dart
@@ -1,6 +1,6 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_getx_palette_diary/src/controller/profile_controller.dart';
+import 'package:flutter_getx_palette_diary/src/widget/profile_box.dart';
 import 'package:get/get.dart';
 
 class Profile extends GetView<ProfileController> {
@@ -27,67 +27,7 @@ class Profile extends GetView<ProfileController> {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 5.0),
-          child: Container(
-            padding:
-                const EdgeInsets.symmetric(vertical: 15.0, horizontal: 10.0),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              border: Border.all(color: Colors.black, width: 2.0),
-              borderRadius: BorderRadius.circular(15.0),
-            ),
-            width: double.infinity,
-            height: 200.0,
-            child: Row(
-              children: <Widget>[
-                Obx(() {
-                  return ClipOval(
-                    child: controller.profileImagePath.value.isNotEmpty
-                        ? Image.file(
-                            File(controller.profileImagePath.value),
-                            width: 100.0,
-                            height: 100.0,
-                            fit: BoxFit.cover,
-                          )
-                        : Container(
-                            color: Colors.blue,
-                            width: 100.0,
-                            height: 100.0,
-                            child: const Center(
-                              child: Icon(
-                                Icons.camera_alt,
-                                color: Colors.white,
-                              ),
-                            ),
-                          ),
-                  );
-                }),
-                const SizedBox(width: 20.0),
-                const Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      'Information',
-                      style: TextStyle(
-                          fontSize: 20.0, fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(height: 30.0),
-                    Text(
-                      '▪ 이름 : 유저 이름',
-                      style: TextStyle(fontSize: 20.0),
-                    ),
-                    SizedBox(height: 20.0),
-                    Text(
-                      '▪ Email : email@email.com',
-                      style: TextStyle(fontSize: 20.0),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-        ),
+        ProfileBox(controller: controller),
         Padding(
           padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
           child: Column(
