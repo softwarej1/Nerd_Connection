3d0557fd22dd1e94828a2064a7186d8d776c99eb
kangsangjun <sangjunk>
2023-11-17 12:12:11 +0900

    11_16 ..

--- lib/src/controller/home_controller.dart
+++ lib/src/controller/home_controller.dart
@@ -9,17 +9,21 @@ import 'package:image_picker/image_picker.dart';
 import 'package:photo_manager/photo_manager.dart';
 
 class HomeController extends GetxController {
+  //calendar
   Rx<DateTime> headerDate = DateTime.now().obs;
   Rx<Size> calendarSize = Size.zero.obs;
   GlobalKey calendarKey = GlobalKey();
   GlobalKey calendarHeaderKey = GlobalKey();
   Rx<Size> calendarHeaderSize = Size.zero.obs;
+  //profile
   var profileImagePath = ''.obs;
   Rx<File?> profileSelectedImage = Rx<File?>(null);
   final picker = ImagePicker();
   final Rxn<AssetEntity> _selectedImage = Rxn<AssetEntity>();
+  //write
   AssetEntity? get writeSelectedImage => _selectedImage.value;
 
+//calendar 캘린더와 캘린더 헤더의 크기 계산, 함수로 전달
   void onCalendarCreated(PageController pageController) {
     SchedulerBinding.instance.addPostFrameCallback(
       (_) {
@@ -35,6 +39,7 @@ class HomeController extends GetxController {
     );
   }
 
+//calendar GlobalKey에 연결된 위젯 박스 크기 계산 -> 반환
   Size? getRenderBoxSize(GlobalKey key) {
     if (key.currentContext != null) {
       var renderBox = key.currentContext!.findRenderObject() as RenderBox;
@@ -44,14 +49,17 @@ class HomeController extends GetxController {
     return null;
   }
 
+//Write() 이동
   void handleFabPress() {
     Get.to(() => const Write());
   }
 
+  // ProfileModify() 이동
   void pfmgo() {
     Get.to(() => const ProfileModify());
   }
 
+//겔러리 이동, 선택
   Future<void> pickImage() async {
     final pickedFile =
         await ImagePicker().pickImage(source: ImageSource.gallery);
