dcb8bb3f57f166fbaca518903adbdd20abafdf70
ksy <soyeon001117@gmail.com>
2023-12-20 22:28:07 +0900

    make gitignore

--- lib/src/repository/user_repository.dart
+++ lib/src/repository/user_repository.dart
@@ -1,15 +1,15 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_getx_palette_diary/src/model/user.dart';
 
+import '../utils/api_url.dart';
+
 class UserRepository {
   final dio = Dio();
 
   Future<User?> loginApi(Map<String, dynamic> json) async {
     try {
       print(json);
-      return dio
-          .post('http://13.124.21.82:8080/users/login', data: json)
-          .then((response) {
+      return dio.post(ApiUrls.loginUrl, data: json).then((response) {
         print(response.statusCode);
         print(response.data);
         if (response.statusCode == 200) {
@@ -25,10 +25,8 @@ class UserRepository {
 
   Future<void> putUsers(Map<String, dynamic> json) async {
     try {
-      dio
-          .put("http://13.124.21.82:8080/users/login/${json["id"]}", data: json)
-          .then((response) {
-        print(response.statusCode); // 성공시 201
+      dio.put(ApiUrls.loginUrl, data: json).then((response) {
+        print(response.statusCode);
         if (response.statusCode == 201) {
         } else {
           // exception
---
+++ lib/src/utils/api_url.dart
@@ -0,0 +1,3 @@
+class ApiUrls {
+  static String loginUrl = 'http://13.124.21.82:8080/users/login';
+}
